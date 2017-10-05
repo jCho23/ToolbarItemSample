3b70af166f015a2ddd047f482e76fb56f80ac730
June Cho <junecho.ucb@gmail.com>
2017-10-05 15:55:18 -0700

    Added EmptyListPage

---
+++ ToolbarItemSample/EmptyListPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ToolbarItemSample
+{
+    public partial class EmptyListPage : ContentPage
+    {
+        public EmptyListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
