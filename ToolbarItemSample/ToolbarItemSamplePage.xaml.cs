00c10336428effdfe1a02be196ed9e6669273762
June Cho <junecho.ucb@gmail.com>
2017-10-05 15:40:48 -0700

    Initial Commit

---
+++ ToolbarItemSample/ToolbarItemSamplePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ToolbarItemSample
+{
+    public partial class ToolbarItemSamplePage : ContentPage
+    {
+        public ToolbarItemSamplePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
